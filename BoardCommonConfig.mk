dcc4ba3e0c570fd91f85e81b64c4b30a4acf0cbe
Pierpaolo Pernici <pierpaolo@gmail.com>
2012-08-19 22:13:26 +0200

    Fixing releasetools for galaxys2: disable backuptool for AOKP

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -61,8 +61,9 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 BOARD_FLASH_BLOCK_SIZE := 4096
 
 # Releasetools
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/galaxys2-common/releasetools/galaxys2_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/galaxys2-common/releasetools/galaxys2_img_from_target_files
+AOKP_BUILD := true
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/galaxys2-common/releasetools/galaxys2_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/samsung/galaxys2-common/releasetools/galaxys2_img_from_target_files
 
 # Graphics
 BOARD_EGL_CFG := device/samsung/galaxys2-common/configs/egl.cfg
